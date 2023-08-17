6da3bfcb34ae54b0ed641197ba784cd6246fe681
Suyash <135426582+SuyashChougale@users.noreply.github.com>
2023-08-17 18:21:50 +0530

    formatting and commenting done

--- model.py
+++ model.py
@@ -1,62 +1,75 @@
+from nltk.stem import PorterStemmer,WordNetLemmatizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_distances
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.tokenize import word_tokenize,sent_tokenize
+import string
+
+
 suy = open(r"suyash.txt","r",errors="ignore")
 vai = open(r"Vaibhav.txt","r",errors="ignore")
-
+# Reading the 2 required files for comparison
 df1 = suy.read()
 df2 = vai.read()
 
-import string
-
-from nltk.tokenize import word_tokenize,sent_tokenize
-
+# Tokenizing text into sentences
 sents1 = sent_tokenize(df1)
 sents2 = sent_tokenize(df2)
-from nltk.stem import PorterStemmer,WordNetLemmatizer
 
+#importing tools for stemming and lemmatization
 stemmer = PorterStemmer()
 lemmer = WordNetLemmatizer()
-
+# preprocessing each sentence of first file
 for i in range(len(sents1)):
+    # removing the punctuations from the sentences
     sents1[i] = sents1[i].translate(str.maketrans('','',string.punctuation))
+    # tokenizing sentence into words
     words = word_tokenize(sents1[i])
+
     for j in range(len(words)):
+        # stemming and lemmatizing each word of sentence
         words[j] = stemmer.stem(words[j])
         words[j] = lemmer.lemmatize(words[j])
+    # joining the stemmed and lemmatized words to form the sentence again
     sents1[i] = ' '.join(words) 
+
+
+# preprocessing each sentence of second file
 for i in range(len(sents2)):
+    # removing the punctuations from the sentences
     sents2[i] = sents2[i].translate(str.maketrans('','',string.punctuation))
+    # tokenizing sentence into words
     words = word_tokenize(sents2[i])
     for j in range(len(words)):
+        # stemming and lemmatizing each word of sentence
         words[j] = stemmer.stem(words[j])
         words[j] = lemmer.lemmatize(words[j])
+    # joining the stemmed and lemmatized words to form the sentence again
     sents2[i] = ' '.join(words) 
-# df1 = ' '.join(sents1)
-# df2 = ' '.join(sents2)
-##########################################################
-#countvectorizer
-from sklearn.feature_extraction.text import CountVectorizer
+
+# Method 1
+#importing countvectorizer to vectorize the sentences
+
 vec = CountVectorizer()
+# storing vectors for each sentence 
 vec1 = vec.fit_transform(sents1,sents2)
 
-from sklearn.metrics.pairwise import cosine_similarity
+# checking similarity between both texts
 score = cosine_similarity(vec1[0],vec1[1])
-
+dist1 = cosine_distances(vec1[0],vec1[1])
 print(score)
-##########################################################
-from sklearn.feature_extraction.text import TfidfVectorizer
+print(dist1)
+
+# method 2
 
 tfidf = TfidfVectorizer()
+# storing vectors for each sentence 
 vec2 = tfidf.fit_transform(sents1,sents2)
-# vec2 = tfidf.fit_transform(sents2)
-score2 = cosine_similarity(vec2[0],vec2[1])
-# num_pairs = (len(score2))*(len(score2)-1)/2
-# avg = score2.sum()/num_pairs
-print(score2)
-# *************************************************************
-from sklearn.metrics.pairwise import cosine_distances
-# vec1 = vec.fit_transform(sents1)
-# vec2 = vec.fit_transform(sents2)
 
+# checking similarity between both texts
+score2 = cosine_similarity(vec2[0],vec2[1])
 dist2 = cosine_distances(vec2[0],vec2[1])
-dist1 = cosine_distances(vec1[0],vec1[1])
-print(dist1)
+print(score2)
 print(dist2)
+
